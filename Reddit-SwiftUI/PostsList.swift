3f697986e1ba4d322164aac56ee9f8c7b7e31e91
Erik Lasky <laskye25@gmail.com>
2019-10-08 07:21:29 -0400

    update typename to match associated type

--- Reddit-SwiftUI/PostsList.swift
+++ Reddit-SwiftUI/PostsList.swift
@@ -38,7 +38,7 @@ struct PostsList: View {
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct PostList_Previews : PreviewProvider {
     static var previews: some View {
         PostsList(listingViewModel: ListingViewModel(service: RedditService()))
     }
